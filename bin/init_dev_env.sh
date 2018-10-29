d9cee38e9d668c273c251383543ef2291431edc6
Mirko Kämpf <mirko.kaempf@gmail.com>
2018-10-29 18:39:41 +0100

    prepare migration

---
+++ bin/init_dev_env.sh
@@ -0,0 +1,10 @@
+# Load latest Docker image for Cloudera Quick Start 
+#-----------------------------------------------------------------------------------------
+#docker pull cloudera/quickstart:latest
+
+# Run Quickstart (default)
+
+
+# Mount Project-Home in: /home/workspace
+export PROJECT_HOME=/GITHUB.cloudera.internal/NeuralCrawler/AutomaticLabelIndexer
+echo "Mount the following project folder to the VM : " $PROJECT_HOME
